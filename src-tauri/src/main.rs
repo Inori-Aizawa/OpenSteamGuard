b733ea37a16b7d9452ba9c7e16ee6ac66287c776
inori aizawa <inoriaizawa@proton.me>
2022-11-13 20:22:23 +0100

    Using events now

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,11 +2,16 @@
     all(not(debug_assertions), target_os = "windows"),
     windows_subsystem = "windows"
 )]
+#[macro_use]
+extern crate anyhow;
 #[path = "./steam/accountmanager.rs"]
 mod accountmanager;
 use accountmanager::Manifest;
 use accountmanager::ManifestEntry;
 use steamguard::{ExposeSecret, SteamGuardAccount};
+use steamguard::{UserLogin,LoginError,Confirmation};
+use tauri::Window;
+use log::*;
 #[path = "./steam/errors.rs"]
 mod errors;
 #[path = "./steam/encryption.rs"]
@@ -18,28 +23,87 @@ extern crate lazy_static;
 
 lazy_static! {
     static ref MANIFEST: Manifest = get_configs_with_password();
+    static ref PASSWORD: String = input("Enter password: ");
 }
 
 
 
 // remember to call `.manage(MyState::default())`
-#[tauri::command]
-fn get_auth_codes() -> Vec<String>{
-    let mut configs = &MANIFEST;
+fn get_auth_codes() -> Vec<(String, String, String)>{
+    let configs = &MANIFEST;
+
     let mut auth_codes = Vec::new();
+    //if the accounts are not loaded yet
+    if configs.get_all_loaded().is_empty() {
+        auth_codes.push(("error".to_string(), "accounts not loaded".to_string(), "-1".to_string()));
+        return auth_codes;
+    }
     for config in &configs.entries {
         let account = configs.get_account(&config.account_name).unwrap().lock().unwrap().clone();
         let code = generate_code_for_account(account);
-        println!("code: {:?}", code);
+        auth_codes.push((config.account_name.to_string(), code,config.steam_id.to_string()));
     }
+
     return auth_codes;
 
 }
+#[tauri::command]
+fn heartbeat(window: Window){
+    window.emit("auth_codes", get_auth_codes()).unwrap();
+} 
+#[tauri::command]
+fn get_trade_confirmations(account_name: String, password: String) -> Vec<(String, String, String)>
+{
+    //if either the account name or password is empty, return an error
+    if account_name.is_empty() || password.is_empty() {
+        return vec![("error".to_string(), "account name or password is empty".to_string(), "-1".to_string())];
+    }
+    println!("Getting trade confirmations for {}", account_name);
+
+    let mut confirmations: Vec<(String, String, String)> = Vec::new();
+    let mut account = get_steamguard_acc(account_name.to_string());
+    let mut session = login_to_steam(account_name.to_string(), password.to_string(), &account);
+    if session.is_err() {
+        confirmations.push(("error".to_string(), "login failed".to_string(), "-3".to_string()));
+        println!("Login failed: {:?}", session.err().unwrap());
+        return confirmations;
+    }
+    account.set_session(session.unwrap());
+    loop {
+		match account.get_trade_confirmations() {
+			Ok(confs) => {
+				for confirm in confs {
+                    confirmations.push((confirm.id.to_string(),confirm.description,confirm.creator.to_string()));
+                }
+                if confirmations.len() > 0 {
+                    println!("Found {} confirmations", confirmations.len());
+                    break;
+                }
+                else {
+                    println!("No confirmations found");
+                    return vec![("error".to_string(), "No confirmations found".to_string(), "-2".to_string())];
+
+                }
+			}
+			Err(_) => {
+                
+			    println!("failed to get trade confirmations, asking user to log in");
+			}
+		}
+	}
+    return confirmations;
+}
 
 fn main() {
-
+    let _ = &PASSWORD;
+    //wait for the user to enter their password
+    while PASSWORD.is_empty() {
+        std::thread::sleep(std::time::Duration::from_millis(100));
+    }
+  
+    
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![get_auth_codes])
+        .invoke_handler(tauri::generate_handler![heartbeat, get_trade_confirmations])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
@@ -48,17 +112,26 @@ fn get_configs_with_password() -> accountmanager::Manifest
     let path = dirs::home_dir().unwrap().join(".config/steamguard-cli/maFiles/manifest.json");
     println!("path: {:?}", path);
     let mut accounts = accountmanager::Manifest::load(&path).unwrap();
-    let password = input("Enter password: ");
+    //send a message to the frontend to get the password
+    
+    let password = PASSWORD.to_string();
     accounts.submit_passkey(Some(password.to_string()));
     if accounts.load_accounts().is_err() {
         //return empty manifest
         println!("error loading accounts");
-        return accountmanager::Manifest::default();
+        //exit the program
+        std::process::exit(1);
+        return accounts;
     }
-
-
     return accounts;
 }
+fn get_steamguard_acc(account: String) -> SteamGuardAccount{
+    let configs = &MANIFEST;
+    let account = configs.get_account(&account).unwrap().lock().unwrap().clone();
+    return account;
+}
+
+
 fn get_configs() -> accountmanager::Manifest
 {
     let path = dirs::home_dir().unwrap().join(".config/steamguard-cli/maFiles/manifest.json");
@@ -95,4 +168,45 @@ pub fn input<'a>(s: impl Into<Option<&'a str>>) -> String {
 	std::io::stdin().read_line(&mut val).expect("Something went wrong with the input.");
 	
 	return val[0..val.len() - 1].to_string();
+}
+
+fn login_to_steam(	username: String,
+	password: String,
+	account: &SteamGuardAccount
+) -> anyhow::Result<steamguard::steamapi::Session> {
+	// TODO: reprompt if password is empty
+	let mut login = UserLogin::new(username, password);
+	let mut loops = 0;
+	loop {
+		match login.login() {
+			Ok(s) => {
+				return Ok(s);
+			}
+			Err(LoginError::Need2FA) => match account {
+                a => {
+					let server_time = steamguard::steamapi::get_server_time()?.server_time;
+					login.twofactor_code = a.generate_code(server_time);
+				}
+			},
+			Err(LoginError::NeedCaptcha { captcha_gid }) => {
+				debug!("need captcha to log in");
+                println!("need captcha to log in: https://steamcommunity.com/public/captcha.php?gid={}", &captcha_gid);
+				login.captcha_text = input(Some("Enter captcha: "));
+			}
+			Err(LoginError::NeedEmail) => {
+				println!("You should have received an email with a code.");
+				print!("Enter code: ");
+				login.email_code = input(None);
+			}
+			Err(r) => {
+				error!("Fatal login result: {:?}", r);
+				bail!(r);
+			}
+		}
+		loops += 1;
+		if loops > 2 {
+			error!("Too many loops. Aborting login process, to avoid getting rate limited.");
+			bail!("Too many loops. Login process aborted to avoid getting rate limited.");
+		}
+	}
 }
--- src/main.js
+++ src/main.js
@@ -1,17 +1,66 @@
+const { listen, emit } = window.__TAURI__.event;
 const { invoke } = window.__TAURI__.tauri;
 
-let greetInputEl;
-let greetMsgEl;
+listen("auth_codes", (event) => {
+  console.log(event.payload);
+  codes = event.payload;
+    //loop through codes and display them
+  //clear the greet-mgs
+  document.getElementById("codes-msg").innerHTML = "";
+  codes.forEach(element => {
+    //create a new div
+    let newDiv = document.createElement("div");
+    //create a new text node
+    let newContent = document.createTextNode(element[0] + " : " + element[1]);
+    //add the text node to the newly created div
+    newDiv.appendChild(newContent);
+    //add the newly created element and its content into the DOM
+    let currentDiv = document.getElementById("codes-msg");
 
-async function greet() {
+    currentDiv.appendChild(newDiv);
+
+  });
+});
+
+function notify(message) {
+  emit("auth_codes", { "hello": "world" });
+
+}
+async function heartbeat() {
   // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-  console.log(await invoke("get_auth_codes"));
+  let codes = await invoke("heartbeat");
+
+}
+//when the page loads
+window.onload = function () {
+
+  //get the button
+  let button = document.getElementById("confirmations-btn");
+  //add an event listener to the button
+  button.addEventListener("click", get_trade_confirmations);
+
 }
 
-window.addEventListener("DOMContentLoaded", () => {
-  greetInputEl = document.querySelector("#greet-input");
-  greetMsgEl = document.querySelector("#greet-msg");
-  document
-    .querySelector("#greet-button")
-    .addEventListener("click", () => greet());
-});
+
+async function get_trade_confirmations() {
+  let confirmations = await invoke("get_trade_confirmations", { accountName: document.getElementById("confirm_user").value, password: document.getElementById("confirm_pass").value });
+  console.log(confirmations);
+  document.getElementById("confirmations-msg").innerHTML = "";
+  confirmations.forEach(element => {
+    //create a new div
+    let newDiv = document.createElement("div");
+    //create a new text node
+    let newContent = document.createTextNode(element[0] + " : " + element[1]);
+    //add the text node to the newly created div
+    newDiv.appendChild(newContent);
+    //add the newly created element and its content into the DOM
+    let currentDiv = document.getElementById("confirmations-msg");
+
+    currentDiv.appendChild(newDiv);
+
+  });
+}
+// get_trade_confirmations()
+//call greet every 5 seconds
+setInterval(heartbeat, 1000);
+heartbeat()
